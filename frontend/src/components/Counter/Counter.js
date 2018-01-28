77d4aa2526b3cc3f7fa6cae00ac4bc68bbe6a365
ma <210181238@qq.com>
2018-01-28 21:39:58 +0800

    counter

--- frontend/src/components/Counter/Counter.js
+++ frontend/src/components/Counter/Counter.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import { Layout, Button, Pagination } from 'antd';
+import { Button } from 'antd';
 import styles from './Counter.css';
-
-const { Header, Footer, Sider, Content } = Layout;
 
 const Count = ({ counter, dispatch }) => {
   return (
     <div>
-      <Layout className="layout">
-        <Header>
-          header
-        </Header>
-        <Sider>
-          sider
-        </Sider>
-        <Content>
-          <div className={styles.record}>Highest Record: {counter.record}</div>
-          <div className={styles.current}>Current: {counter.current}</div>
-          <div className={styles.buttons}>
-            <Button type="default" onClick={() => { dispatch({ type: 'counter/add' }); }}>+</Button>
-            <Button type="dashed" onClick={() => { dispatch({ type: 'counter/adddelay' }); }}>+ (delay)</Button>
-          </div>
-        </Content>
-        <Footer>
-          footer
-        </Footer>
-      </Layout>
+      <div className={styles.record}>Highest Record: {counter.record}</div>
+      <div className={styles.current}>Current: {counter.current}</div>
+      <div className={styles.buttons}>
+        <Button type="default" onClick={() => { dispatch({ type: 'counter/add' }); }}>+</Button>
+        <Button type="dashed" onClick={() => { dispatch({ type: 'counter/adddelay' }); }}>+ (delay)</Button>
+      </div>
     </div>
   );
 };
