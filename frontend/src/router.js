8f771ffc7cfe3cc451a3e40d1dbbed1dde008ea6
ma <210181238@qq.com>
2018-03-16 21:32:51 +0800

    change router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
-import CountPage from './routes/CountPage';
+import { Redirect, Router, Route, Switch } from 'dva/router';
 import NewsPage from './routes/News/NewsPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
-        <Route path="/counter" exact component={CountPage} />
         <Route path="/news" exact component={NewsPage} />
+        <Route path="/" exact render={() => <Redirect to="/news" />} />
       </Switch>
     </Router>
   );
